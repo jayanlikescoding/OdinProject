0b61405a567fd220dcdfd2c91f2a59740e37a7ba
Jayan Almadi <jayanalmadi@Jayans-MacBook-Pro-14.local>
2024-07-08 23:40:07 +0530

    Testing

--- Foundations-JS/javascript.js
+++ Foundations-JS/javascript.js
@@ -5,4 +5,14 @@ message=hello;
 // alert(message);
 // alert(hello);
 
-const myBirthday = "25.11.2006";
+// const myBirthday = "25.11.2006";
+
+let myInt = 5;
+typeof myInt;
+
+let myNumber = "74";
+myNumber=Number(myNumber)
+typeof myNumber;
+
+
+console.log(10+20);
